f8e3276682b3b0c04e0f03cac4ff084034ac809b
wangye <wy891608948@gmail.com>
2018-04-15 23:01:51 +0800

    提交成功

---
+++ ssm-model/src/main/java/com.wangye.ssm_parent.model/User.java
@@ -0,0 +1,8 @@
+package src.main.java.com.wangye.ssm_parent.model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Boolean sex;
+    private String address;
+}
